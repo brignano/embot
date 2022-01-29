462578d997faad0f8c544d9035e780951318ed32
Anthony Brignano <abrignano@comcast.net>
2022-01-29 15:59:51 -0500

    add comments

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ def main():
     env.reset()
     done = False
     while not done:
+        # show gameplay
         env.render()
+
+        # generate a random sample action
         action = env.action_space.sample()
-        ob, rew, done, info = env.step(action)
         print("Action ", action)
-        print("Image ", ob.shape, "Reward ", rew, "Done? ", done)
+
+        # preform the action
+        img, rew, done, info = env.step(action)
+        print("Image ", img.shape, "Reward ", rew, "Done? ", done)
         print("Info ", info)
 
 
