ee7e73f2ebe13663b681d7cbd9a7ab455826e4b4
Anthony Brignano <abrignano@comcast.net>
2022-01-29 18:24:45 -0500

    add neat-python

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
-import retro
+import pickle
+
+import cv2
 import neat
+import numpy as np
+import retro
+
+env = retro.make('SuperMarioBros-Nes')
+
+imgarray = []
 
 
-def main():
-    env = retro.make(game='SuperMarioBros-Nes')
-    env.reset()
-    done = False
-    while not done:
-        # show gameplay
-        env.render()
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        ob = env.reset()
+        ac = env.action_space.sample()
 
-        # generate a random sample action
-        action = env.action_space.sample()
-        print("Action ", action)
+        inx, iny, inc = env.observation_space.shape
 
-        # preform the action
-        img, rew, done, info = env.step(action)
-        print("Image ", img.shape, "Reward ", rew, "Done? ", done)
-        print("Info ", info)
+        inx = int(inx / 8)
+        iny = int(iny / 8)
+
+        net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
+
+        current_max_fitness = 0
+        fitness_current = 0
+        frame = 0
+        counter = 0
+
+        done = False
+
+        while not done:
+
+            env.render()
+            frame += 1
+            ob = cv2.resize(ob, (inx, iny))
+            ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
+            ob = np.reshape(ob, (inx, iny))
+
+            imgarray = np.ndarray.flatten(ob)
+            nn_output = net.activate(imgarray)
+
+            ob, rew, done, info = env.step(nn_output)
+
+            fitness_current += rew
+
+            if fitness_current > current_max_fitness:
+                current_max_fitness = fitness_current
+                counter = 0
+            else:
+                counter += 1
+
+            if done or counter == 250:
+                done = True
+                print(genome_id, fitness_current)
+
+            genome.fitness = fitness_current
 
 
-if __name__ == "__main__":
-    main()
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                     neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                     'config-feedforward')
+
+p = neat.Population(config)
+
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+p.add_reporter(neat.Checkpointer(10))
+
+winner = p.run(eval_genomes)
+
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner, output, 1)
