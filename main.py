cfb4c30da790c0ade76c30c5facc8466abb14499
Anthony Brignano <abrignano@comcast.net>
2022-02-05 00:14:36 -0500

    refactor main method

--- main.py
+++ main.py
@@ -1,76 +1,44 @@
-import pickle
+import multiprocessing
+import os
 
-import cv2
-import neat
-import numpy as np
-import retro
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
 
-env = retro.make('SuperMarioBros-Nes')
-
-
-def eval_genomes(genomes, config):
-    for genome_id, genome in genomes:
-        ob = env.reset()
-        ac = env.action_space.sample()
-
-        inx, iny, inc = env.observation_space.shape
-
-        inx = int(inx / 8)
-        iny = int(iny / 8)
-
-        net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
-
-        current_max_fitness = 0
-        fitness_current = 0
-        frame = 0
-        counter = 0
-
-        done = False
-
-        while not done:
-
-            env.render()
-            frame += 1
-            ob = cv2.resize(ob, (inx, iny))
-            ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
-            ob = np.reshape(ob, (inx, iny))
-
-            imgarray = np.ndarray.flatten(ob)
-            nn_output = net.activate(imgarray)
-
-            ob, rew, done, info = env.step(nn_output)
-
-            fitness_current += rew
-
-            if fitness_current > current_max_fitness:
-                current_max_fitness = fitness_current
-                counter = 0
-            else:
-                counter += 1
-
-            if done or counter == 250:
-                done = True
-                print(genome_id, fitness_current)
-
-            genome.fitness = fitness_current
-
-
-def main():
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         'config-feedforward')
-
-    p = neat.Population(config)
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-    p.add_reporter(neat.Checkpointer(10))
-
-    winner = p.run(eval_genomes)
-
-    with open('dist/winner.pkl', 'wb') as output:
-        pickle.dump(winner, output, 1)
-
+from utils.callbacks import SaveOnBestTrainingRewardCallback
 
 if __name__ == '__main__':
-    main()
+    env_id = 'CartPole-v1'
+    cpu_count = multiprocessing.cpu_count()
+
+    # We create a separate environment for evaluation
+    eval_env = gym.make(env_id)
+
+    # Create log dir
+    log_dir = '/tmp/gym/'
+    os.makedirs(log_dir, exist_ok=True)
+
+    # Logs will be saved in log_dir/monitor.csv
+    eval_env = Monitor(eval_env, log_dir)
+    vec_env = make_vec_env(env_id, n_envs=cpu_count)
+
+    # Create our model
+    model = PPO('MlpPolicy', vec_env, verbose=0)
+
+    # Random Agent, before training
+    mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10)
+    print(f'Mean reward (pre-training): {mean_reward} +/- {std_reward:.2f}')
+
+    # Create the callback: check every 1000 steps
+    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+
+    # Training the agent
+    n_timesteps = 25000
+    model.learn(n_timesteps, callback=callback)
+    model.save('models/cartpole')
+
+    # Trained Agent, after training
+    mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10)
+    print(f'Mean reward (post-training): {mean_reward} +/- {std_reward:.2f}')
