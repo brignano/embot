d6e667d5b4fb270ea0f4b31e9837cc7d48a7a4b9
Anthony Brignano <abrignano@comcast.net>
2022-02-05 00:19:37 -0500

    rename `main.py` to `train.py`

--- train.py
+++ train.py
@@ -20,23 +20,22 @@ if __name__ == '__main__':
     log_dir = '/tmp/gym/'
     os.makedirs(log_dir, exist_ok=True)
 
-    # Logs will be saved in log_dir/monitor.csv
+    # wrap environment in Monitor
     eval_env = Monitor(eval_env, log_dir)
+
+    # create vector environment for parallel processing
     vec_env = make_vec_env(env_id, n_envs=cpu_count)
 
-    # Create our model
+    # Create the model
     model = PPO('MlpPolicy', vec_env, verbose=0)
 
     # Random Agent, before training
     mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10)
     print(f'Mean reward (pre-training): {mean_reward} +/- {std_reward:.2f}')
 
-    # Create the callback: check every 1000 steps
-    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
-
-    # Training the agent
+    # Train the agent
     n_timesteps = 25000
-    model.learn(n_timesteps, callback=callback)
+    model.learn(n_timesteps, callback=SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir))
     model.save('models/cartpole')
 
     # Trained Agent, after training
